ab045b8ff5694b8c8a78b8f8c184655bd8be1fcd
Jasper Yiu <yiujasper@gmail.com>
2018-11-16 08:55:07 -0800

    f1=0.45; p=0.76; r=0.32

--- erisk_task2.py
+++ erisk_task2.py
@@ -10,34 +10,73 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import AdaBoostClassifier
 
 
 from os import listdir
 from os.path import isfile, join
 
+from textblob import TextBlob
+import string, re
+from nltk.corpus import stopwords
+import nltk, ssl
+
+
+from nltk.stem import PorterStemmer
 
 '''
-Note:
+NOTE:
+The program currently processes all chunks from the training data, combines them in a pandas dataframe,
+fits them using logistic regression/support vector machine/adaboost, and then emits decisions for
+each chunk in the testing data.
 
-The program currently processes both negative and positive exampls from chunk 1 in the training data,
-divides it randomly into its own sets of training data (80% for training, 20% for testing) and then
-tests it on a logistic regression model (dividing the data up for training and testing is for my personal
-testing purposes - not instructions from the project).
 
-It currently achieves 92% accuracy but that is also because 90% of the data is negative (so guessing 
-that everything is negative would yield 90% accuracy but does not tell me anything because I am more
-interested in the positive classification)
+HOW TO RUN:
+From main directory (task 2 - anorexia), run:
+$ python erisk_task2.py
+$ python test/aggregate_results.py -path decisions/ -wsource test/writings-per-subject-all-test.txt
+$ python test/erisk_eval.py -gpath test/risk-golden-truth-test.txt -ppath decisions/usc_global.txt -o 50
 
-- How to deal with this class data imbalance?
-- Is it possible to 
 
+TODO: 
 - Should we build the model based on multiple features (as opposed to just the title/text)? i.e. also
 include features like "number of words / submission"?
 - How do we build the model based on multiple features?
+- How to deal with class data imbalance?
 
 '''
 
-def train_svm():
+
+
+def readfile (file): # processes the XML and returns a single string of all titles + texts for this particular subject
+    result = ''
+
+    dom = ElementTree.parse(file) 
+    tmp_subj_id = dom.find('ID').text
+    writing = dom.findall('WRITING')
+
+    for w in writing:
+        title = w.find('TITLE').text
+        text = w.find('TEXT').text
+        result = result + title + text
+        
+    result = re.sub(r'[^\w\s]','',result) # remove punctuation
+    result = result.lower() # make lowercase
+    #result = TextBlob(result).correct()
+
+    #print(result + '\n----------------------------------------------------------------------------------------------------------------\n')
+
+    return tmp_subj_id, result
+
+
+def build_df():
+
+    blob1 = TextBlob("I hate Mondayy")
+    print(format(blob1.sentiment))
+
+    print(TextBlob("I hate Mondau").correct())
+
 
     # ----------------------------------------------------------------------------------------
     # ----------------------------------- NEGATIVE CHUNKS ------------------------------------
@@ -100,34 +139,87 @@ def train_svm():
     # ------------------------------------ COMBINE CHUNKS ------------------------------------
     # ----------------------------------------------------------------------------------------
     result = pd.concat([result_neg, result_pos])
-    #print(result)
+    print(result)
     #print(type(result.iloc[1]), (result.iloc[1]).shape)
 
 
-    # ---------------------------------------- build model ------------------------------------
-    X_train, X_test, y_train, y_test = train_test_split(result.text, result.target, test_size=0.005) # for personal testing (will not be passed on / used)
-
-    
-
-    X_train = result.loc[:, 'text']
-    y_train = result.loc[:, 'target']
-
-    ''' test print the X_train, X_test, y_train, y_test values
-    print('-------------------- X_train ------------------') # X_train and Y_train must have same columns in order to run logistic regression
-    print(X_train) # shape is (19, 6)
-    print('-------------------- Y_train ------------------')
-    print(y_train) # shape is (19,)
-    print('-------------------- X_test ------------------')
-    print(X_test)
-    print('-------------------- y_test ------------------')
-    #print(y_test, end=' ') # actual test values
-    for x in y_test:
-        print (x,end=' ')
-    #print((y_test).shape)
+    '''
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
     '''
 
+    # --------------------- stop word removal
+    nltk.download("stopwords")
+    stop = stopwords.words('english')
+    #print(stop)
+    result['text'] = result['text'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+    
+
+    # --------------------- common word removal
+    common = list((pd.Series(' '.join(df['text']).split()).value_counts()[:10]).index)
+    print(common)
+    result['text'] = result['text'].apply(lambda x: " ".join(x for x in x.split() if x not in common))
+
+    # --------------------- rare word removal
+    rare = list((pd.Series(' '.join(df['text']).split()).value_counts()[-10:]).index)
+    print(rare)
+    result['text'] = result['text'].apply(lambda x: " ".join(x for x in x.split() if x not in rare))
+    #print((pd.Series(' '.join(df['text']).split()).value_counts()[:10]).index) # Index([u'it', u'the', u'i', u'you', u'a', u'and', u'of', u'to', u'im',u'that'],dtype='object')
+
+
+
+    '''
+    '''
+    # --------------------- stemming
+    st = PorterStemmer()
+    result['text'] = result['text'].apply(lambda x: " ".join([st.stem(word) for word in x.split()])) #result['text'][:5] is a <class 'pandas.core.series.Series'>
+
+
+    print(result)
+
+    return result
+
+def train_adaboost(df):
+        # ---------------------------------------- build model ------------------------------------
+    X_train, X_test, y_train, y_test = train_test_split(df.text, df.target, test_size=0.01) # for personal testing (will not be used in final program)
+
+    
+    X_train = df.loc[:, 'text']
+    y_train = df.loc[:, 'target']
+
     # -
-    cvec = CountVectorizer().fit(X_train)
+    cvec = CountVectorizer(stop_words='english').fit(X_train) # use tf-idf instead?
+    df_train = pd.DataFrame(cvec.transform(X_train).todense(),columns=cvec.get_feature_names())
+    df_test = pd.DataFrame(cvec.transform(X_test).todense(),columns=cvec.get_feature_names())
+
+    model = AdaBoostClassifier(n_estimators=100)
+    model.fit(df_train, y_train)
+
+    
+    prediction = model.predict_proba(df_test)
+    print(prediction) # what the machine predicts is the possibility of class 0 vs. class 1, respectively
+    #print( type(model.predict_proba(df_test)) )
+
+
+    print(model.score(df_test, y_test)) # accuracy of the prediction for personal testing (will not be used in final program)
+    return model, cvec
+
+
+def train_svm(df):
+
+    # ---------------------------------------- build model ------------------------------------
+    X_train, X_test, y_train, y_test = train_test_split(df.text, df.target, test_size=0.01)  # for personal testing (will not be used in final program)
+
+    
+    X_train = df.loc[:, 'text']
+    y_train = df.loc[:, 'target']
+
+    # -
+    cvec = CountVectorizer(stop_words='english').fit(X_train)
     df_train = pd.DataFrame(cvec.transform(X_train).todense(),columns=cvec.get_feature_names())
     df_test = pd.DataFrame(cvec.transform(X_test).todense(),columns=cvec.get_feature_names())
 
@@ -137,105 +229,26 @@ def train_svm():
     
     prediction = model.predict_proba(df_test)
     print(prediction) # what the machine predicts is the possibility of class 0 vs. class 1, respectively
-    #print( type(model.predict_proba(df_test)) )
 
 
     '''
     # ----------- TESTING AND EVALUATION STAGE
-
-    # --- open a file
-    f = open("usc_1.txt", "a")
-
-    # --- loop over predictions and print 
-    for i in range(prediction.shape[0]):
-        if (prediction[i][0] < 0.95 and prediction[i][1] < 0.95 ):
-            f.write()
-
     print(prediction[0][0], prediction[0][1])
-
-
     print(model.predict(df_test)) # what the machine predicts are the test values
-    #print((model.predict(df_test)).shape)
-
     '''
-    print(model.score(df_test, y_test)) # accuracy of the prediction
+
+    print(model.score(df_test, y_test)) # accuracy of the prediction for personal testing (will not be used in final program)
+    return model, cvec
 
 
 
-def train_lr():
-
-    # ----------------------------------------------------------------------------------------
-    # ----------------------------------- NEGATIVE CHUNKS ------------------------------------
-    # ----------------------------------------------------------------------------------------
-
-    chunks = [f for f in listdir('train/negative_examples/')] # negative - 132
-    frames_neg = []
-
-    for c in chunks:
-        path = 'train/negative_examples/' + c
-        negative = [f for f in listdir(path) if isfile(join(path, f))]
-
-        for n in negative:
-            (id, text) = readfile('train/negative_examples/'+c+'/'+n)
-
-            # -- create NEGATIVE dataframe
-            df = {'text' : text}
-            df = pd.DataFrame(data=[df])
-
-            # --
-            frames_neg.append(df)
-
-
-    result_neg = pd.concat(frames_neg)
-
-    # - append target feature
-    target = [0] * result_neg.shape[0]
-    result_neg['target'] = target
-
-
-    # ----------------------------------------------------------------------------------------
-    # ----------------------------------- POSITIVE CHUNKS ------------------------------------
-    # ----------------------------------------------------------------------------------------
-    chunks = [f for f in listdir('train/positive_examples/')] # negative - 132
-    frames_pos = []
-
-    for c in chunks:
-        path = 'train/positive_examples/' + c
-        positive = [f for f in listdir(path) if isfile(join(path, f))]
-
-        for p in positive:
-            (id, text) = readfile('train/positive_examples/'+c+'/'+p)
-
-            # -- create NEGATIVE dataframe
-            df = {'text' : text}
-            df = pd.DataFrame(data=[df])
-
-            # --
-            frames_pos.append(df)
-
-
-    result_pos = pd.concat(frames_pos)
-
-    # - append target feature
-    target = [1] * result_pos.shape[0]
-    result_pos['target'] = target
-
-  
-    # ----------------------------------------------------------------------------------------
-    # ------------------------------------ COMBINE CHUNKS ------------------------------------
-    # ----------------------------------------------------------------------------------------
-    result = pd.concat([result_neg, result_pos])
-    #print(result)
-    #print(type(result.iloc[1]), (result.iloc[1]).shape)
-
+def train_lr(df):
 
     # ---------------------------------------- build model ------------------------------------
-    X_train, X_test, y_train, y_test = train_test_split(result.text, result.target, test_size=0.005)
+    X_train, X_test, y_train, y_test = train_test_split(df.text, df.target, test_size=0.33)  # for personal testing (will not be used in final program)
 
-    
-
-    X_train = result.loc[:, 'text']
-    y_train = result.loc[:, 'target']
+    X_train = df.loc[:, 'text']
+    y_train = df.loc[:, 'target']
 
     ''' test print the X_train, X_test, y_train, y_test values
     print('-------------------- X_train ------------------') # X_train and Y_train must have same columns in order to run logistic regression
@@ -252,6 +265,7 @@ def train_lr():
     '''
 
     # -
+    #cvec = CountVectorizer(stop_words='english').fit(X_train) # alternatively, use TfidfVectorizer()
     cvec = CountVectorizer().fit(X_train)
     df_train = pd.DataFrame(cvec.transform(X_train).todense(),columns=cvec.get_feature_names())
     df_test = pd.DataFrame(cvec.transform(X_test).todense(),columns=cvec.get_feature_names())
@@ -264,57 +278,19 @@ def train_lr():
     #print(prediction) # what the machine predicts is the possibility of class 0 vs. class 1, respectively
     #print( type(model.predict_proba(df_test)) )
 
-
-    '''
-    # ----------- TESTING AND EVALUATION STAGE
-
-    # --- open a file
-    f = open("usc_1.txt", "a")
-
-    # --- loop over predictions and print 
-    for i in range(prediction.shape[0]):
-        if (prediction[i][0] < 0.95 and prediction[i][1] < 0.95 ):
-            f.write()
-
-    print(prediction[0][0], prediction[0][1])
-
-
-    print(model.predict(df_test)) # what the machine predicts are the test values
-    #print((model.predict(df_test)).shape)
-
-    '''
-    print(lr.score(df_test, y_test)) # accuracy of the prediction
-    return model, cvec, lr
+    print(lr.score(df_test, y_test)) # accuracy of the prediction for personal testing (will not be used in final program)
+    return model, cvec
 
 
 
-def readfile (file):
-    result = ''
 
-    dom = ElementTree.parse(file)
-    tmp_subj_id = dom.find('ID').text
-    writing = dom.findall('WRITING')
-
-    for w in writing:
-        title = w.find('TITLE').text
-        text = w.find('TEXT').text
-
-        # print('* {} - {} - {} - {}'.format(title,date,info,text))
-        result = result + title + text
-
-    return tmp_subj_id, result
-
-
-def test_lr(model, cvec, lr):
-    # --- read chunks
-
-
+def test(model, cvec):
     for c in range(10): #process chunk
         text_list = []
         id_list = []
 
         path = 'test/chunk' + str(c+1)
-        test_file = [f for f in listdir(path) if isfile(join(path, f))] # TODO: does it matter that this is not in order?
+        test_file = [f for f in listdir(path) if isfile(join(path, f))] 
         
         # for each subject file in chunk c
         for t in test_file:
@@ -328,42 +304,41 @@ def test_lr(model, cvec, lr):
         #print(result)
 
         X_test = result.loc[:, 'text']
-        #print(X_test.shape)
 
         df_test = pd.DataFrame(cvec.transform(X_test).todense(),columns=cvec.get_feature_names())
         prediction_proba = model.predict_proba(df_test)
         prediction = model.predict(df_test)
-        print(prediction_proba) 
-        #print(model.predict(df_test))
-        #print( (model.predict(df_test))[2] )
-
-        #print(X_test.shape)
-        #print(prediction.shape)
-
-
+        #print(prediction)
 
         # --- open file
-        f = open("usc_" + str(c+1) + ".txt", "a")
+        f = open("decisions/usc_" + str(c+1) + ".txt", "a")
 
         i = 0
         for id in id_list:
-            print(prediction_proba[i][0], prediction_proba[i][1])
-            if (c==9): # chunk 10 - emit a decision regardless
-                if (prediction[i] == 0):
+            #print(prediction_proba[i][0], prediction_proba[i][1])
+
+            #if (c!=9): # emit a decision regardless of prediction score
+            if (c<2 and prediction_proba[i][0] < 0.96 and prediction_proba[i][1] < 0.96): # 0,1,2
+                f.write(id + '\t\t0\n')
+            elif (c<4 and prediction_proba[i][0] < 0.90 and prediction_proba[i][1] < 0.90): # 3
+                f.write(id + '\t\t0\n')
+            elif (c<6 and prediction_proba[i][0] < 0.85 and prediction_proba[i][1] < 0.85): # 4,5
+                f.write(id + '\t\t0\n')
+            elif (c<9 and prediction_proba[i][0] < 0.80 and prediction_proba[i][1] < 0.80): # 6,7 - if less than 97% sure, do not decide
+                f.write(id + '\t\t0\n')
+            else: # emit a decision
+                if (prediction[i] == 0): # if the machine predicted 0 then write 2 to the file
                     f.write(id + '\t\t2\n')
-                else:
+                else: # write 1 because machine predicted 1
                     f.write(id + '\t\t1\n')
-            elif (c>7 and prediction_proba[i][0] < 0.75 and prediction_proba[i][1] < 0.75): # chunk 8,9
-                f.write(id + '\t\t0\n')
-            elif (c>5 and prediction_proba[i][0] < 0.85 and prediction_proba[i][1] < 0.85): # chunk 6,7
-                f.write(id + '\t\t0\n')
-            elif (prediction_proba[i][0] < 0.95 and prediction_proba[i][1] < 0.95): # chunk 3,4,5
-                f.write(id + '\t\t0\n')
-            else:
-                if (prediction[i] == 0):
+
+            '''
+            else: # chunk 9 - emit a decision regardless of prediction score
+                if (prediction[i] == 0): # if the machine predicted 0 then write 2 to the file
                     f.write(id + '\t\t2\n')
-                else:
+                else: # write 1 because machine predicted 1
                     f.write(id + '\t\t1\n')
+            '''
 
             i=i+1
 
@@ -375,30 +350,19 @@ def test_lr(model, cvec, lr):
 # ------------------------------------- MAIN PROGRAM -------------------------------------
 # ----------------------------------------------------------------------------------------
 
-'''
-python erisk_task2.py
-python test/aggregate_results.py -path decisions/ -wsource test/writings-per-subject-all-test.txt
-python test/erisk_eval.py -gpath test/risk-golden-truth-test.txt -ppath decisions/usc_global.txt -o 50
-
-'''
-
 np.set_printoptions(threshold='nan') # --- display whole numpy array
 pd.set_option('display.expand_frame_repr', False) # --- display whole dataframe
 
 
-# ------ Build model using logistic regression
+# ------ Build dataframe using training data
+df = build_df()
 
 
-#model, cvec, lr = train_lr()
-train_svm()
-
-# ------ Test
-#test_lr(model, cvec, lr)
-
-#prediction = model.predict_proba(df_test)
-#print(lr.score(df_test, y_test)) # accuracy of the prediction
+# ------ Training stage - choose one of the following classification algorithms and comment out the others
+model, cvec = train_lr(df) # --- logistic regression
+#model, cvec = train_svm(df) # --- support vector machine
+#model, cvec = train_adaboost(df) # --- ada boost
 
 
-# test
-#(id, result) = readfile('train/negative_examples/chunk1/subject31_1.xml')
-#print(result)
+# ------ Testing stage
+test(model, cvec)
